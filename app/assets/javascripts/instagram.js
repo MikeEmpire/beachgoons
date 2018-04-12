74429ba329187bbd036e51791661e8e11f53d976
Michael Olie <aolie94@gmail.com>
2018-04-12 10:36:29 -0700

    added instafeed to dynamically generate pictures and changed logo font width in header

--- app/assets/javascripts/instagram.js
+++ app/assets/javascripts/instagram.js
@@ -2,7 +2,8 @@ $( document ).on('turbolinks:load', function() {
   var feed = new Instafeed({
     get: 'user',
     userId: '5795008810',
-    accessToken: '689297533.1677ed0.17c203355c474f588f1883a674605488'
+    accessToken: '5795008810.3fcedb7.ae03e963af43404c8c8cbe82a2ada513',
+    template: '<div class="col-md-3"><a href="{{link}}"><img src="{{image}}" /></a></div>'
   });
   feed.run();
   console.log(feed);
