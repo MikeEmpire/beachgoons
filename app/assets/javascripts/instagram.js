2f3abd242e387fc3756cac22ed581490ea27b9d8
Michael Olie <aolie94@gmail.com>
2018-04-12 10:59:22 -0700

    Added standard size to instafeed images with a limit of 8 images

--- app/assets/javascripts/instagram.js
+++ app/assets/javascripts/instagram.js
@@ -3,7 +3,9 @@ $( document ).on('turbolinks:load', function() {
     get: 'user',
     userId: '5795008810',
     accessToken: '5795008810.3fcedb7.ae03e963af43404c8c8cbe82a2ada513',
-    template: '<div class="col-md-3"><a href="{{link}}" target="_blank"><img src="{{image}}" /></a></div>'
+    template: '<div class="col-md-3"><a href="{{link}}" target="_blank"><img class={{orientation}} src="{{image}}" /></a></div>',
+    resolution: 'standard_resolution',
+    limit: 8
   });
   feed.run();
   console.log(feed);
